af868c7db5c28b32bb05f3cceab3930f0f068adc
sparshrestha <15346503+sparshrestha@users.noreply.github.com>
2025-11-08 14:36:34 +0545

    both ways of transpose(matrix) function

--- attention_pure_python.py
+++ attention_pure_python.py
@@ -4,11 +4,29 @@
 # Demonstrates unmasked, causal masked, and padding masked attention, and visualizes
 # attention weights as heatmaps using matplotlib.
 
-import math
-import matplotlib.pyplot as plt
 
-def transpose(matrix):
+def transpose_short (matrix):
     return [list(row) for row in zip(*matrix)]
 
+
+def transpose_long (matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    result = []
+
+    # loop over columns of original
+    for c in range(cols):
+        new_row = []
+        # loop over rows of original
+        for r in range(rows):
+            new_row.append(matrix[r][c])
+        result.append(new_row)
+
+    return result
+
+
+M = [[1, 2, 3], [4, 5, 6]]
+print(transpose_short(M))
+
 M = [[1, 2], [3, 4], [5, 6]]
-print(transpose(M))
+print(transpose_long(M))
